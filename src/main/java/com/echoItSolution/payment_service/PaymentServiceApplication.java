89eaf02d40574785c3880f18d262ce97a5ef5de2
ariftxt <arif.ali@xyloop.com>
2025-09-13 12:36:36 +0530

    added common-app dependencies in services

--- src/main/java/com/echoItSolution/payment_service/PaymentServiceApplication.java
+++ src/main/java/com/echoItSolution/payment_service/PaymentServiceApplication.java
@@ -3,7 +3,9 @@ package com.echoItSolution.payment_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"com.echoItSolution.*" })
 @SpringBootApplication
 @EnableDiscoveryClient
 public class PaymentServiceApplication {
